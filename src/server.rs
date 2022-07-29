a8361cd42bec44993461b7d6083f4e45082ee7c4
einisto <79069176+einisto@users.noreply.github.com>
2022-07-29 03:34:30 +0300

    Key-based authentication & critical fix to server's async behaviour
    * 8 character long alphanumeric key authentication (server-side)
    * Modified integration test to skip the previously mentioned key authentication
    * Fixed a previously made typo, which caused the server to await until a single
      client's connection was done
    * Simplified the structure of client.rs & server.rs a lot, also improved comments

--- src/server.rs
+++ src/server.rs
@@ -1,9 +1,11 @@
 use local_ip_address::local_ip;
+use rand::{distributions::Alphanumeric, thread_rng, Rng};
 use std::{
     error::Error,
     fs::read_dir,
     net::{IpAddr, SocketAddr},
     path::PathBuf,
+    process::exit,
     str::FromStr,
     time::Duration,
 };
@@ -11,7 +13,10 @@ use tokio::{
     self,
     fs::File,
     io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader, BufWriter},
-    net::TcpListener,
+    net::{
+        tcp::{ReadHalf, WriteHalf},
+        TcpListener,
+    },
     time::timeout,
 };
 
@@ -21,17 +26,26 @@ pub async fn listen(
     buffersize: usize,
     localhost: bool,
     timeout_duration: u64,
+    use_testing_key: bool,
 ) -> Result<(), Box<dyn Error>> {
     let addr = match localhost {
         true => SocketAddr::new(IpAddr::from_str("127.0.0.1")?, port),
         false => SocketAddr::new(local_ip()?, port),
     };
+    // Use weak access key for integration testing, otherwise 8 char alphanumeric
+    let access_key = match use_testing_key {
+        true => "test".to_string(),
+        false => generate_key(),
+    };
 
     let listener = TcpListener::bind(addr).await?;
     println!("[+] Listening on {}", addr);
+    println!("[+] Access key: {}", access_key);
 
     loop {
+        // The first loop iteration would take the ownership without cloning
         let alt_fileroot = fileroot.clone();
+        let alt_access_key = access_key.clone();
 
         let (mut socket, addr) =
             match timeout(Duration::from_secs(timeout_duration), listener.accept()).await {
@@ -44,144 +58,71 @@ pub async fn listen(
                     break;
                 }
             };
-        println!("\n[+] New client: {}", addr);
 
-        let task = tokio::spawn(async move {
+        println!("\n[NEW] {}: Connected", addr);
+
+        tokio::spawn(async move {
             let (reader, writer) = socket.split();
             let mut reader = BufReader::new(reader);
             let mut writer = BufWriter::new(writer);
 
             let mut vec_buf = Vec::new();
 
+            // ACK ready-to-receive status
+            send_msg(&mut writer, "ACK\n").await?;
+
+            // Check access key
+            if !check_access_key(&mut reader, &mut writer, &mut vec_buf, &alt_access_key).await? {
+                println!("[FIN] {}: Incorrect access key", addr);
+                return Ok::<(), Box<dyn Error + Send + Sync>>(());
+            }
+
             // Send buffersize
-            writer.write_all(buffersize.to_string().as_bytes()).await?;
-            writer.flush().await?;
+            send_msg(&mut writer, (buffersize.to_string() + "\n").as_str()).await?;
 
-            // Read ACK
-            let _bytes_read = reader.read_until(b'\n', &mut vec_buf).await?;
-            let msg = String::from_utf8(vec_buf.clone())?;
-            if msg.trim() != "ACK" {
-                let e: Box<dyn Error + Send + Sync> =
-                    format!("ACK not received (buffersize)").into();
-                return Err::<(), Box<dyn Error + Send + Sync>>(e);
-            } else {
-                vec_buf.clear();
+            // ACK buffersize
+            if recv_msg_string(&mut reader, &mut vec_buf).await? != "ACK" {
+                return Ok::<(), Box<dyn Error + Send + Sync>>(());
             }
 
-            let (metadata_list, file_amount) = get_metadata(alt_fileroot.clone()).await?;
-            if file_amount == 0 {
-                println!(
-                    "[-] No files available inside fileroot {:#?}, shutting down",
-                    alt_fileroot
-                );
-                // No files available, request connection termination
-                writer.write_all(b"FIN\n").await?;
-                writer.flush().await?;
-
-                // Read FIN
-                let _bytes_read = reader.read_until(b'\n', &mut vec_buf).await?;
-                let msg = String::from_utf8(vec_buf.clone())?;
-                if msg.trim() != "FIN" {
-                    let e: Box<dyn Error + Send + Sync> =
-                        format!("ACK not received (server-side termination)").into();
-                    return Err::<(), Box<dyn Error + Send + Sync>>(e);
-                } else {
-                    // Empty error as error's reason is already logged with println
-                    let e: Box<dyn Error + Send + Sync> = "".into();
-                    return Err::<(), Box<dyn Error + Send + Sync>>(e);
+            // Send metadata
+            match handle_metadata(&mut reader, &mut writer, &mut vec_buf, &alt_fileroot, &addr)
+                .await?
+            {
+                None => println!("[DATA] {}: Ready to serve files", addr),
+                Some(err_msg) => {
+                    println!("{}", err_msg);
+                    exit(0x0100);
                 }
-            } else {
-                // Send file amount
-                let msg = file_amount.to_string() + "\n";
-                writer.write_all(msg.as_bytes()).await?;
-                writer.flush().await?;
             }
 
-            // Read ACK
-            let _bytes_read = reader.read_until(b'\n', &mut vec_buf).await?;
-            let msg = String::from_utf8(vec_buf.clone())?;
-            if msg.trim() != "ACK" {
-                let e: Box<dyn Error + Send + Sync> =
-                    format!("ACK not received (file amount)").into();
-                return Err::<(), Box<dyn Error + Send + Sync>>(e);
-            } else {
-                vec_buf.clear();
-            }
-
-            // Send file metadata
-            for file in &metadata_list {
-                // Newline as delimiter between instances
-                let msg = format!("{}:{}\n", file.1, file.0);
-                writer.write_all(msg.as_bytes()).await?;
-                writer.flush().await?;
-            }
-
-            // Handle file request(s)
-            println!("[+] Ready to serve files");
-            loop {
-                let bytes_read = reader.read_until(b'\n', &mut vec_buf).await?;
-
-                if bytes_read == 0 {
-                    println!("[-] File request never received or client crashed");
-                    vec_buf.clear();
-                    break;
-                } else {
-                    let msg = String::from_utf8(vec_buf.clone())?;
-                    vec_buf.clear();
-
-                    if msg.trim() == "FIN" {
-                        println!("[+] FIN received, terminating connection with {}", addr);
-                        break;
-                    }
-
-                    let mut input_path = alt_fileroot.clone();
-                    input_path.push(msg.trim());
-
-                    println!("\n[+] File requested: {:#?}", input_path);
-                    let mut file = File::open(input_path.clone()).await?;
-                    let mut remaining_data = file.metadata().await?.len();
-                    let mut filebuf = vec![0u8; buffersize];
-
-                    while remaining_data != 0 {
-                        let read_result = file.read(&mut filebuf);
-                        match read_result.await {
-                            Ok(n) => {
-                                writer.write_all(&filebuf).await?;
-                                writer.flush().await?;
-                                remaining_data = remaining_data - n as u64;
-                            }
-                            _ => {}
-                        }
-                    }
-                }
-
-                // Read ACK
-                let _bytes_read = reader.read_until(b'\n', &mut vec_buf).await?;
-                let msg = String::from_utf8(vec_buf.clone())?;
-                if msg.trim() != "ACK" {
-                    let e: Box<dyn Error + Send + Sync> =
-                        format!("ACK not received (single file's data)").into();
-                    return Err::<(), Box<dyn Error + Send + Sync>>(e);
-                } else {
-                    println!("[+] File transfer successfully done");
-                    vec_buf.clear();
+            // Send filedata
+            match handle_file_reqs(
+                &mut reader,
+                &mut writer,
+                &mut vec_buf,
+                &alt_fileroot,
+                &buffersize,
+                &addr,
+            )
+            .await?
+            {
+                None => println!("[FIN] {}: Disconnected", addr),
+                Some(err_msg) => {
+                    println!("{}", err_msg);
+                    exit(0x0100);
                 }
             }
 
             Ok::<(), Box<dyn Error + Send + Sync>>(())
         });
-
-        match task.await? {
-            Ok(_) => continue,
-            Err(_) => break,
-        }
     }
 
     Ok(())
 }
 
 async fn get_metadata(
-    fileroot: PathBuf,
+    fileroot: &PathBuf,
 ) -> Result<(Vec<(String, u64)>, usize), Box<dyn Error + Send + Sync>> {
     let mut metadata = Vec::<(String, u64)>::new();
     let paths = read_dir(fileroot)?;
@@ -201,4 +142,143 @@ async fn get_metadata(
     let amount = metadata.len();
 
     Ok((metadata, amount))
+}
+
+async fn handle_metadata(
+    reader: &mut BufReader<ReadHalf<'_>>,
+    writer: &mut BufWriter<WriteHalf<'_>>,
+    buf: &mut Vec<u8>,
+    fileroot: &PathBuf,
+    addr: &SocketAddr,
+) -> Result<Option<String>, Box<dyn Error + Send + Sync>> {
+    let (metadata_list, file_amount) = get_metadata(fileroot).await?;
+
+    // Terminate if fileroot is empty
+    if file_amount == 0 {
+        send_msg(writer, "FIN\n").await?;
+        return Ok(Some(format!(
+            "[-] No files inside {:#?}, shutting host down",
+            fileroot
+        )));
+    }
+
+    // Send metadata amount
+    send_msg(writer, (file_amount.to_string() + "\n").as_str()).await?;
+
+    // ACK metadata amount
+    if recv_msg_string(reader, buf).await? != "ACK" {
+        return Ok(Some(format!(
+            "[ERROR] {}: No confirmation of metadata amount",
+            addr
+        )));
+    }
+
+    // Send metadata
+    for file in &metadata_list {
+        send_msg(writer, format!("{}:{}\n", file.1, file.0).as_str()).await?;
+    }
+
+    Ok(None)
+}
+
+fn generate_key() -> String {
+    thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(8)
+        .map(char::from)
+        .collect::<String>()
+}
+
+async fn send_msg(
+    writer: &mut BufWriter<WriteHalf<'_>>,
+    msg: &str,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    writer.write_all(msg.as_bytes()).await?;
+    writer.flush().await?;
+    Ok(())
+}
+
+async fn recv_msg_string(
+    reader: &mut BufReader<ReadHalf<'_>>,
+    buf: &mut Vec<u8>,
+) -> Result<String, Box<dyn Error + Send + Sync>> {
+    let bytes_received = reader.read_until(b'\n', buf).await?;
+
+    if bytes_received == 0 {
+        let e: Box<dyn Error + Send + Sync> =
+            format!("No message received or client crashed").into();
+        return Err::<String, Box<dyn Error + Send + Sync>>(e);
+    }
+
+    let msg = String::from_utf8(buf.clone())?;
+    buf.clear();
+
+    Ok(msg.trim().to_string())
+}
+
+async fn check_access_key(
+    reader: &mut BufReader<ReadHalf<'_>>,
+    writer: &mut BufWriter<WriteHalf<'_>>,
+    buf: &mut Vec<u8>,
+    access_key: &String,
+) -> Result<bool, Box<dyn Error + Send + Sync>> {
+    if recv_msg_string(reader, buf).await? != *access_key {
+        send_msg(writer, "FIN\n").await?;
+        return Ok(false);
+    } else {
+        send_msg(writer, "ACK\n").await?;
+        recv_msg_string(reader, buf).await?; // Might be a bit unnecessary ACK
+        return Ok(true);
+    }
+}
+
+async fn handle_file_reqs(
+    reader: &mut BufReader<ReadHalf<'_>>,
+    writer: &mut BufWriter<WriteHalf<'_>>,
+    buf: &mut Vec<u8>,
+    fileroot: &PathBuf,
+    buffersize: &usize,
+    addr: &SocketAddr,
+) -> Result<Option<String>, Box<dyn Error + Send + Sync>> {
+    loop {
+        // Receive filename or termination request
+        let req = recv_msg_string(reader, buf).await?;
+
+        if req == "FIN" {
+            break;
+        }
+
+        let mut input_path = fileroot.clone();
+        input_path.push(req);
+
+        println!("\n[REQ] {}: {:#?}", addr, input_path);
+        let mut file = File::open(input_path.clone()).await?;
+        let mut remaining_data = file.metadata().await?.len();
+        let mut filebuf = vec![0u8; *buffersize];
+
+        // Serve the file itself
+        while remaining_data != 0 {
+            let read_result = file.read(&mut filebuf);
+            match read_result.await {
+                Ok(n) => {
+                    writer.write_all(&filebuf).await?;
+                    writer.flush().await?;
+                    remaining_data = remaining_data - n as u64;
+                }
+                _ => {}
+            }
+        }
+
+        // ACK file
+        if recv_msg_string(reader, buf).await? != "ACK" {
+            return Ok(Some(format!(
+                "[ERROR] {}: No confirmation of file {:#?}",
+                addr, input_path
+            )));
+        } else {
+            println!("[ACK] {}: File finished successfully", addr);
+        }
+    }
+
+    Ok(None)
 }
